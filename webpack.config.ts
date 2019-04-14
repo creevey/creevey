e75802c7916d35e4b8029368ebe8ebb6b58aca3e
wKich <nflazzer@gmail.com>
2019-04-14 21:39:07 +0500

    add react-ui

--- webpack.config.ts
+++ webpack.config.ts
@@ -15,8 +15,19 @@ const config: Configuration = {
         test: /\.tsx?/,
         exclude: /node_modules/,
         use: "babel-loader"
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", { loader: "css-loader", options: { modules: "global" } }]
+      },
+      {
+        test: /\.(woff2?|eot|png)$/,
+        use: "file-loader"
       }
     ]
+  },
+  resolve: {
+    extensions: [".tsx", ".ts", ".jsx", ".js"]
   },
   plugins: [new HtmlWebpackPlugin({ template: "./src/client/index.html" })],
   devServer: {
