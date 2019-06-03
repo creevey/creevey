5da4a8d5e8ea36ce8e1f45cd1eeed2cdfb33935d
wKich <nflazzer@gmail.com>
2019-06-03 15:37:01 +0500

    feat(client): add `BlendView` component

--- src/client/ImagesView/BlendView.tsx
+++ src/client/ImagesView/BlendView.tsx
@@ -1,9 +1,32 @@
 import React from "react";
+import { css } from "@emotion/core";
 import { ViewProps } from "./ImagesView";
 
-export class BlendView extends React.Component<ViewProps> {
-  render() {
-    const { url, actual, diff, expect } = this.props;
-    return null;
-  }
+export function BlendView({ url, actual, expect }: ViewProps) {
+  return (
+    <div
+      css={css`
+        display: inline-block;
+        position: relative;
+        margin: 20px;
+        filter: invert(100%);
+      `}
+    >
+      <img
+        src={`${url}/${actual}`}
+        css={css`
+          position: absolute;
+          border: 1px solid #419d14;
+        `}
+      />
+      <img
+        src={`${url}/${expect}`}
+        css={css`
+          vertical-align: top;
+          border: 1px solid #d9472b;
+          mix-blend-mode: difference;
+        `}
+      />
+    </div>
+  );
 }
