3cde3a7050478bbd1887fe832251036ee36ad2c1
wKich <nflazzer@gmail.com>
2019-09-09 10:11:00 +0500

    refactor: move unit tests into separate dir
    Former-commit-id: 124c43a1a9ad6ccbe666dc44a539202b7370f39b

--- tests/unit/client/helpers.test.ts
+++ tests/unit/client/helpers.test.ts
@@ -1,7 +1,7 @@
 import { describe, it } from "mocha";
 import { expect } from "chai";
-import { toogleChecked } from "../../src/client/helpers";
-import { Suite } from "../../src/client/CreeveyContext";
+import { toogleChecked } from "../../../src/client/helpers";
+import { Suite } from "../../../src/client/CreeveyContext";
 
 function mockTest() {
   return {
