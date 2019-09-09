4f97fd6a5e12e1a06836337299aece8ef0890dcd
wKich <nflazzer@gmail.com>
2019-09-09 10:56:27 +0500

    feat: add storybook decorator
    Former-commit-id: b24a55a707cc3f39ee34f1ffb6ea644a8a209f6b

---
+++ src/storybook.tsx
@@ -0,0 +1,46 @@
+import React from "react";
+import addons, { makeDecorator, StoryContext } from "@storybook/addons";
+
+type StoryDidMountCallback = (context: StoryContext) => void;
+
+interface CreeveyStoryWrapperProps {
+  context: StoryContext;
+  onDidMount: StoryDidMountCallback;
+}
+
+class CreeveyStoryWrapper extends React.Component<CreeveyStoryWrapperProps> {
+  componentDidMount() {
+    const { context, onDidMount } = this.props;
+    onDidMount(context);
+  }
+
+  render() {
+    return this.props.children;
+  }
+}
+
+addons.getChannel().once("setStories", ({ stories }) => {
+  // @ts-ignore
+  window.getStories = callback => callback(stories);
+});
+
+export default () => {
+  let storyDidMountCallback: StoryDidMountCallback = () => {};
+  // @ts-ignore
+  window.selectStory = (storyId: string, kind: string, name: string, callback: StoryDidMountCallback) => {
+    storyDidMountCallback = callback;
+    addons.getChannel().emit("setCurrentStory", { storyId, name, kind });
+  };
+  return makeDecorator({
+    name: "withCreevey",
+    parameterName: "creevey",
+    // TODO what data exists in settings argument?
+    wrapper(getStory, context, _settings) {
+      return (
+        <CreeveyStoryWrapper context={context} onDidMount={storyDidMountCallback}>
+          {getStory(context)}
+        </CreeveyStoryWrapper>
+      );
+    }
+  });
+};
