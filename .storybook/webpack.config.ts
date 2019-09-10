fc94d128237760cbda380246ce406c75735542a2
wKich <nflazzer@gmail.com>
2019-09-10 16:04:50 +0500

    build: update deps
    Former-commit-id: 23a3cbcf63ee95f0cb37445961af793245762243

--- .storybook/webpack.config.ts
+++ .storybook/webpack.config.ts
@@ -7,8 +7,5 @@ export default async ({ config }: { config: Configuration }) => {
   config.module.rules[0].test = /\.(mjs|(j|t)sx?)$/;
   config.module.rules[0].use[0].options.overrides = [{ presets: ["@emotion/babel-preset-css-prop"] }];
 
-  // /\.css$/ -> css-loader
-  // TODO Remove after update @skbkontur/react-ui
-  config.module.rules[2].use[1].options.modules = "global";
   return config;
 };
