23deb9283374aa1aae38890f978727444be2771a
wKich <nflazzer@gmail.com>
2019-09-18 16:45:50 +0500

    test: update stories format
    Former-commit-id: 0305d7bf525e6a9c29068beac0d8e2047c3b1fc4

--- stories/ImagesView.stories.tsx
+++ stories/ImagesView.stories.tsx
@@ -1,15 +1,14 @@
 import React from "react";
 
-import { storiesOf } from "@storybook/react";
-
 import { BlendView, SideBySideView, SlideView, SwapView } from "../src/client/ImagesView";
 
 import octocatExpect from "./fixtures/octocat-expect.png";
 import octocatDiff from "./fixtures/octocat-diff.png";
 import octocatActual from "./fixtures/octocat-actual.png";
 
-storiesOf("ImagesView", module)
-  .add("BlendView", () => <BlendView expect={octocatExpect} diff={octocatDiff} actual={octocatActual} />)
-  .add("SideBySideView", () => <SideBySideView expect={octocatExpect} diff={octocatDiff} actual={octocatActual} />)
-  .add("SlideView", () => <SlideView expect={octocatExpect} diff={octocatDiff} actual={octocatActual} />)
-  .add("SwapView", () => <SwapView expect={octocatExpect} diff={octocatDiff} actual={octocatActual} />);
+export default { title: "ImagesViews" };
+
+export const Blend = () => <BlendView expect={octocatExpect} diff={octocatDiff} actual={octocatActual} />;
+export const SideBySide = () => <SideBySideView expect={octocatExpect} diff={octocatDiff} actual={octocatActual} />;
+export const Slide = () => <SlideView expect={octocatExpect} diff={octocatDiff} actual={octocatActual} />;
+export const Swap = () => <SwapView expect={octocatExpect} diff={octocatDiff} actual={octocatActual} />;
