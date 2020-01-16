ddf3615ca36e1f36ff02a348ad2cec0bc819a304
wKich <nflazzer@gmail.com>
2020-01-16 10:05:26 +0500

    fix: 🐛 add hint for images preview

--- src/client/CreeveyView/ResultsPage/ImagePreview.tsx
+++ src/client/CreeveyView/ResultsPage/ImagePreview.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { css } from '@emotion/core';
 import Button from '@skbkontur/react-ui/Button';
+import Hint from '@skbkontur/react-ui/Hint';
 
 const IMG_SIZE = 64;
 const DIAG_LENGTH = (2 * (IMG_SIZE - 8) ** 2) ** (1 / 2);
@@ -16,52 +17,54 @@ export function ImagePreview({ isActive, onClick, imageName, url }: ImageSwapPro
   const handleClick = (): void => onClick(imageName);
 
   return (
-    <Button checked={isActive} onClick={handleClick}>
-      <span
-        css={css`
-          display: flex;
-          text-align: center;
-          align-items: center;
-          justify-content: center;
-        `}
-      >
-        <img
+    <Hint text={imageName}>
+      <Button checked={isActive} onClick={handleClick}>
+        <span
           css={css`
-            max-height: ${IMG_SIZE}px;
-            width: ${IMG_SIZE}px;
-            overflow: hidden;
-
-            ${isActive
-              ? 'transform: translateY(-1px); &:active { transform: translateY(1px); }'
-              : '&:active { transform: translateY(-1px); }'}
-
-            &::before {
-              content: ' ';
-              display: block;
-              height: ${IMG_SIZE - 8}px;
-              width: ${IMG_SIZE - 8}px;
-              margin: 4px;
-              background-color: #fff;
-              background-image: linear-gradient(
-                  45deg,
-                  rgba(0, 0, 0, 0) ${DIAG_LENGTH / 2 - 0.5}px,
-                  #e5e5e5 ${DIAG_LENGTH / 2 - 0.5}px,
-                  #e5e5e5 ${DIAG_LENGTH / 2 + 0.5}px,
-                  rgba(0, 0, 0, 0) ${DIAG_LENGTH / 2 + 0.5}px
-                ),
-                linear-gradient(
-                  315deg,
-                  rgba(0, 0, 0, 0) ${DIAG_LENGTH / 2 + 0.2}px,
-                  #e5e5e5 ${DIAG_LENGTH / 2 + 0.2}px,
-                  #e5e5e5 ${DIAG_LENGTH / 2 + 1.2}px,
-                  rgba(0, 0, 0, 0) ${DIAG_LENGTH / 2 + 1.2}px
-                );
-            }
+            display: flex;
+            text-align: center;
+            align-items: center;
+            justify-content: center;
           `}
-          src={url}
-          alt={imageName}
-        />
-      </span>
-    </Button>
+        >
+          <img
+            css={css`
+              max-height: ${IMG_SIZE}px;
+              width: ${IMG_SIZE}px;
+              overflow: hidden;
+
+              ${isActive
+                ? 'transform: translateY(-1px); &:active { transform: translateY(1px); }'
+                : '&:active { transform: translateY(-1px); }'}
+
+              &::before {
+                content: ' ';
+                display: block;
+                height: ${IMG_SIZE - 8}px;
+                width: ${IMG_SIZE - 8}px;
+                margin: 4px;
+                background-color: #fff;
+                background-image: linear-gradient(
+                    45deg,
+                    rgba(0, 0, 0, 0) ${DIAG_LENGTH / 2 - 0.5}px,
+                    #e5e5e5 ${DIAG_LENGTH / 2 - 0.5}px,
+                    #e5e5e5 ${DIAG_LENGTH / 2 + 0.5}px,
+                    rgba(0, 0, 0, 0) ${DIAG_LENGTH / 2 + 0.5}px
+                  ),
+                  linear-gradient(
+                    315deg,
+                    rgba(0, 0, 0, 0) ${DIAG_LENGTH / 2 + 0.2}px,
+                    #e5e5e5 ${DIAG_LENGTH / 2 + 0.2}px,
+                    #e5e5e5 ${DIAG_LENGTH / 2 + 1.2}px,
+                    rgba(0, 0, 0, 0) ${DIAG_LENGTH / 2 + 1.2}px
+                  );
+              }
+            `}
+            src={url}
+            alt={imageName}
+          />
+        </span>
+      </Button>
+    </Hint>
   );
 }
