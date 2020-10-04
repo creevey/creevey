8e674b4109248d7f8253209c0c2417ebe390bd72
Lelchik <matsenko.olga@gmail.com>
2020-10-04 13:40:14 +0500

    test: 💍 add page header tests

---
+++ stories/PageHeader.stories.tsx
@@ -0,0 +1,52 @@
+import React from 'react';
+import { PageHeader } from '../src/client/CreeveyView/ResultsPage/PageHeader';
+import { noop } from '../src';
+
+export default {
+  title: 'PageHeader',
+};
+
+export const Simple = (): JSX.Element => (
+  <PageHeader
+    title={['chrome', 'title', '1']}
+    showViewModes
+    viewMode={'side-by-side'}
+    onImageChange={noop}
+    onViewModeChange={noop}
+  />
+);
+
+export const WithError = (): JSX.Element => (
+  <PageHeader
+    title={['chrome', 'title', '2']}
+    errorMessage={'errorMessage'}
+    showViewModes={false}
+    viewMode={'swap'}
+    onImageChange={noop}
+    onViewModeChange={noop}
+  />
+);
+export const WithImagePreview = (): JSX.Element => (
+  <PageHeader
+    title={['chrome', 'title', '3']}
+    showViewModes={false}
+    viewMode={'swap'}
+    onImageChange={noop}
+    onViewModeChange={noop}
+    images={{ click: { actual: '1' }, idle: { actual: '2', error: 'error' } }}
+    imagesWithError={['idle']}
+  />
+);
+
+export const Full = (): JSX.Element => (
+  <PageHeader
+    showTitle
+    title={['chrome', 'title', '4']}
+    showViewModes
+    viewMode={'swap'}
+    onImageChange={noop}
+    onViewModeChange={noop}
+    images={{ click: { actual: '1', error: 'error' }, idle: { actual: '2', error: 'error' } }}
+    imagesWithError={['idle', 'click']}
+  />
+);
