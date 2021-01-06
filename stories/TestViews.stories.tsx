261c80111cca4badf6caa864ff43e0da4de8cdf8
Egor Pogadaev <pogadaev.e@skbkontur.ru>
2021-01-06 14:21:09 +0500

    test: add screenshots with ignored elements

--- stories/TestViews.stories.tsx
+++ stories/TestViews.stories.tsx
@@ -1,5 +1,8 @@
 import React, { FunctionComponent } from 'react';
 import { styled } from '@storybook/theming';
+import { Story } from '@storybook/react';
+
+import { CreeveyStory } from '../src/types';
 
 export default {
   title: 'TestViews',
@@ -39,8 +42,8 @@ const Wrapper = styled.span({
 });
 
 const ComponentChunk: FunctionComponent = (props) => (
-  <ChunkContainer>
-    <Wrapper>{props.children}</Wrapper>
+  <ChunkContainer data-tid="Tile">
+    <Wrapper data-tid="TileContent">{props.children}</Wrapper>
   </ChunkContainer>
 );
 
@@ -63,3 +66,9 @@ export const ViewportFit = (): JSX.Element => <ChunkTiles size={2} offset={0} />
 export const Overflow = (): JSX.Element => <ChunkTiles size={6} offset={0} />;
 export const ViewportFitOffset = (): JSX.Element => <ChunkTiles size={2} offset={4} />;
 export const OverflowOffset = (): JSX.Element => <ChunkTiles size={6} offset={4} />;
+export const IgnoreElements: Story & CreeveyStory = () => <ChunkTiles size={3} offset={0} />;
+IgnoreElements.parameters = {
+  creevey: {
+    ignoreElements: '[data-tid=Tile]:nth-of-type(even) [data-tid=TileContent]',
+  },
+};
