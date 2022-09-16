bf568589ed6a13b45143d4408d5cf932c3ed06d9
Egor Pogadaev <pogadaev.e@skbkontur.ru>
2022-09-16 13:15:37 +0500

    chore: export new entities from index

--- src/index.ts
+++ src/index.ts
@@ -3,3 +3,5 @@ export * from './client/addon/withCreevey';
 export * from './client/addon/readyForCapture';
 export { loadStories as browserStoriesProvider } from './server/storybook/providers/browser';
 export { loadStories as nodejsStoriesProvider } from './server/storybook/providers/nodejs';
+export { loadStories as hybridStoriesProvider } from './server/storybook/providers/hybrid';
+export * from './server/parser';
