37706efbb49dd5bd1d6ec06821fac52480a0e132
Egor Pogadaev <pogadaev.e@skbkontur.ru>
2022-09-16 10:06:18 +0500

    fix: prevent importing browser-specific code to node

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,3 @@
 export * from './types';
-export * from './client/addon/withCreevey';
-export * from './client/addon/readyForCapture';
 export { loadStories as browserStoriesProvider } from './server/storybook/providers/browser';
 export { loadStories as nodejsStoriesProvider } from './server/storybook/providers/nodejs';
