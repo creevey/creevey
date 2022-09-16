737670e18aa5d0ce416fe12b765406116b453e31
Egor Pogadaev <pogadaev.e@skbkontur.ru>
2022-09-16 10:21:43 +0500

    fix: browser-node regexp parameters transfering

--- src/shared.ts
+++ src/shared.ts
@@ -1,6 +1,6 @@
 import { Parameters } from '@storybook/api';
-import { mapValues, mergeWith } from 'lodash';
-import { SetStoriesData, StoriesRaw } from './types';
+import { mapValues, mergeWith, cloneDeepWith } from 'lodash';
+import { SetStoriesData, StoriesRaw, CreeveyStoryParams, StoryInput } from './types';
 
 // NOTE: Copy-paste from storybook/api
 export const combineParameters = (...parameterSets: Parameters[]): Parameters =>
@@ -26,4 +26,77 @@ export const denormalizeStoryParameters = ({
       storyData.parameters as Parameters,
     ),
   })) as StoriesRaw;
+};
+
+interface SerializedRegExp {
+  __regexp: true;
+  source: string;
+  flags: string;
+}
+
+export const isSerializedRegExp = (exp: unknown): exp is SerializedRegExp => {
+  return typeof exp === 'object' && exp !== null && Reflect.get(exp, '__regexp') === true;
+};
+
+export const serializeRegExp = (exp: RegExp): SerializedRegExp => {
+  const { source, flags } = exp;
+  return {
+    __regexp: true,
+    source,
+    flags,
+  };
+};
+
+export const deserializeRegExp = ({ source, flags }: SerializedRegExp): RegExp => {
+  return new RegExp(source, flags);
+};
+
+export const serializeRawStories = (stories: StoriesRaw): StoriesRaw => {
+  return mapValues(stories, (storyData) => {
+    const creevey = storyData.parameters.creevey as CreeveyStoryParams | undefined;
+    if (creevey?.skip) {
+      return {
+        ...storyData,
+        parameters: {
+          ...storyData.parameters,
+          creevey: {
+            ...creevey,
+            skip: cloneDeepWith(creevey.skip, (value) => {
+              if (value instanceof RegExp) {
+                return serializeRegExp(value);
+              }
+              return undefined;
+            }) as CreeveyStoryParams['skip'],
+          },
+        },
+      };
+    }
+    return storyData;
+  });
+};
+
+export const deserializeRawStories = (stories: StoriesRaw): StoriesRaw => {
+  return mapValues(stories, deserializeStory);
+};
+
+export const deserializeStory = (story: StoryInput): StoryInput => {
+  const creevey = story.parameters.creevey as CreeveyStoryParams | undefined;
+  if (creevey?.skip) {
+    return {
+      ...story,
+      parameters: {
+        ...story.parameters,
+        creevey: {
+          ...creevey,
+          skip: cloneDeepWith(creevey.skip, (value) => {
+            if (isSerializedRegExp(value)) {
+              return deserializeRegExp(value);
+            }
+            return undefined;
+          }) as CreeveyStoryParams['skip'],
+        },
+      },
+    };
+  }
+  return story;
 };
