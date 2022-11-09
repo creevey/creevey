a051b49502467fbef3c197480ef765afe378fe5a
Egor Pogadaev <pogadaev.e@skbkontur.ru>
2022-11-09 10:20:03 +0500

    chore: move presets to common dir

--- preset/index.js
+++ preset/index.js
@@ -1,9 +1,9 @@
 /* eslint-disable node/no-missing-require, @typescript-eslint/no-unsafe-assignment */
 
 module.exports = {
-  ...require('./lib/cjs/client/addon/preset'),
+  ...require('../lib/cjs/client/addon/preset'),
   /** @type {(entry: string[]) => string[]} */
   config(entry) {
-    return [...entry, require.resolve('./lib/esm/client/addon/decorator')];
+    return [...entry, require.resolve('../lib/esm/client/addon/decorator')];
   },
 };
