9482472904c76703bd4d38b1cbf7c73858f3d453
Dmitriy Lazarev <w@kich.dev>
2024-07-19 17:45:35 +0500

    Add storybook cli, fix starting storybook
    Signed-off-by: Dmitriy Lazarev <w@kich.dev>

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,14 +1,23 @@
+// TODO Update config to 7.x
 export default {
   stories: ['../stories/**/*.stories.@(md|ts)x'],
   addons: [
-    '@storybook/addon-postcss',
     '@storybook/addon-essentials',
     {
       name: './../src/client/addon/preset',
       options: { clientPort: 8000, creeveyPreExtract: './scripts/babel-register' },
     },
   ],
+  // TODO: Do we need it?
   features: {
     previewCsfV3: true,
+  },
+  framework: {
+    name: '@storybook/react-webpack5',
+    options: {
+      builder: {
+        useSWC: true,
+      },
+    },
   },
 };
