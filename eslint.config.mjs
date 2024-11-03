0674d26993b7e2154d75d9d103cd2bf3bef123ec
Dmitriy Lazarev <w@kich.dev>
2024-11-03 17:24:55 +0500

    Update eslint deps
    Signed-off-by: Dmitriy Lazarev <w@kich.dev>

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,7 +1,6 @@
 import globals from 'globals';
 import js from '@eslint/js';
 import tseslint from 'typescript-eslint';
-// @ts-expect-error There is no types in this package
 import react from 'eslint-plugin-react';
 import reactHooks from 'eslint-plugin-react-hooks';
 import prettierRecommended from 'eslint-plugin-prettier/recommended';
@@ -11,7 +10,6 @@ const config = tseslint.config(
   { ignores: ['dist/*', 'report/*', 'scripts/*'] },
   js.configs.recommended,
   prettierRecommended,
-  /* eslint-disable-next-line */
   react.configs.flat.recommended,
   ...tseslint.configs.strictTypeChecked,
   ...tseslint.configs.stylisticTypeChecked,
--- src/server/worker/worker.ts
+++ src/server/worker/worker.ts
@@ -180,7 +180,7 @@ async function saveImages(imageDir: string, images: { name: string; data: Buffer
 function serializeError(error: unknown): string {
   if (!error) return 'Unknown error';
   if (error instanceof Error) return error.stack ?? error.message;
-  return error instanceof Object ? JSON.stringify(error) : String(error);
+  return typeof error === 'object' ? JSON.stringify(error) : (error as string);
 }
 
 function hasDisconnected(str: string | null | undefined): boolean {
