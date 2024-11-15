b516aacc869586d1befe09567d7022d8bd468d69
Dmitriy Lazarev <w@kich.dev>
2024-11-15 17:08:51 +0500

    Get npm registry for docker file, move storybook deps to devDeps
    Signed-off-by: Dmitriy Lazarev <w@kich.dev>

--- src/server/playwright/docker-file.ts
+++ src/server/playwright/docker-file.ts
@@ -1,9 +1,16 @@
 import semver from 'semver';
+import { exec } from 'shelljs';
 
 // TODO Support custom docker images
-// TODO Support nexus
 export function playwrightDockerFile(browser: string, version: string): string {
   const sv = semver.coerce(version);
+
+  let npmRegistry;
+  try {
+    npmRegistry = exec('npm config get registry', { silent: true }).stdout.trim();
+  } catch {
+    /* noop */
+  }
 
   return `
 FROM mcr.microsoft.com/playwright:v${sv?.format() ?? version}
@@ -12,7 +19,12 @@ WORKDIR /creevey
 
 RUN echo "{ \\"type\\": \\"module\\" }" > package.json && \\
     echo "import { ${browser} as browser } from 'playwright-core';" >> index.js && \\
-    echo "const ws = await browser.launchServer({ port: 4444, wsPath: 'creevey' })" >> index.js && \\
+    echo "const ws = await browser.launchServer({ port: 4444, wsPath: 'creevey' })" >> index.js && \\${
+      npmRegistry
+        ? `
+    echo "registry=${npmRegistry}" > .npmrc && \\`
+        : ''
+    }
     npm i playwright-core${sv ? `@${sv.format()}` : ''}
 
 EXPOSE 4444
