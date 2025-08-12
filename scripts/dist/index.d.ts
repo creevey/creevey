801319d3e5062a035266fb39f5971fd115a82eec
Dmitriy Lazarev <w@kich.dev>
2025-08-12 14:01:20 +0500

    Publish 0.10.5
    Signed-off-by: Dmitriy Lazarev <w@kich.dev>

--- scripts/dist/index.d.ts
+++ scripts/dist/index.d.ts
@@ -1,6 +1,6 @@
 /// <reference types="../types/chai" />
 /// <reference types="../types/global" />
 export * from './types.js';
-export { loadStories as browserStoriesProvider } from './server/storybook/providers/browser.js';
-export { loadStories as hybridStoriesProvider } from './server/storybook/providers/hybrid.js';
+export { loadStories as browserStoriesProvider } from './server/providers/browser.js';
+export { loadStories as hybridStoriesProvider } from './server/providers/hybrid.js';
 export * from './server/testsFiles/parser.js';
