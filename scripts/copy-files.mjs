c3f54c86ccf72ac1e9d82292ded3ad80cd146efb
Dmitriy Lazarev <w@kich.dev>
2025-08-14 21:22:59 +0500

    fix export esm preview
    Signed-off-by: Dmitriy Lazarev <w@kich.dev>

---
+++ scripts/copy-files.mjs
@@ -0,0 +1,8 @@
+import { copyFile } from 'node:fs/promises';
+
+await copyFile('./scripts/dist/index.d.ts', './dist/index.d.ts');
+await copyFile('./scripts/dist/selenium.d.ts', './dist/selenium.d.ts');
+await copyFile('./scripts/dist/playwright.d.ts', './dist/playwright.d.ts');
+await copyFile('./src/server/playwright/index-source.mjs', './dist/server/playwright/index-source.mjs');
+await copyFile('./scripts/dist/client/addon/preset.mjs', './dist/client/addon/preset.mjs');
+await copyFile('./scripts/dist/client/addon/preview.mjs', './dist/client/addon/preview.mjs');
---
+++ scripts/dist/client/addon/preset.mjs
@@ -0,0 +1,4 @@
+export const previewAnnotations = [require.resolve('./preview')];
+
+// TODO: Disable manager for now until fully reworked
+// export const managerEntries = [require.resolve('./manager')];
